d7b4d12a0cef3b5bf2947e0b655d210a91966a7f
Maarten van Gompel <proycon@anaproy.nl>
2020-09-28 14:10:48 +0200

    more logging on error

--- eng_ASR/transcriber.sh
+++ eng_ASR/transcriber.sh
@@ -5,6 +5,38 @@ scratchdir=$2
 outdir=$3
 resourcedir=$4
 
+fatalerror() {
+    echo "-----------------------------------------------------------------------" >&2
+    echo "FATAL ERROR: $*" >&2
+    echo "-----------------------------------------------------------------------" >&2
+    rm $scratchdir/${file_id}.wav 2>/dev/null
+    if [ ! -z "$target_dir" ]; then
+        echo "PATH=$PATH" >&2
+        echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >&2
+        echo "KALDI_ROOT=$KALDI_ROOT" >&2
+        echo "[Index of $target_dir]" >&2
+        du -ah $target_dir >&2
+        echo "[End of index]">&2
+        echo "[Output of intermediate log]" >&2
+        cat $target_dir/intermediate/log >&2
+        echo "[End output of intermediate log]">&2
+        echo "[Output of intermediate lium logs]" >&2
+        cat $target_dir/intermediate/data/ALL/liumlog/*.log >&2
+        echo "[End output of intermediate log]">&2
+        echo "[Output of other intermediate logs]" >&2
+        cat $target_dir/intermediate/data/ALL/log/*.log >&2
+        echo "[End output of other intermediate log]">&2
+        echo "[Output of kaldi decode logs]" >&2
+        cat $target_dir/intermediate/decode//log/decode*log >&2
+        echo "[End of kaldi decode logs]" >&2
+        if [ ! -z "$debug" ]; then
+            echo "(cleaning intermediate files after error)">&2
+            rm -Rf $target_dir
+        fi
+    fi
+    exit 2
+}
+
 cd $resourcedir
 suffix=$(LC_CTYPE=C tr -d -c '[:alnum:]' </dev/urandom | head -c 15)
 for inputfile in $inputdir/*; do
@@ -17,7 +49,7 @@ for inputfile in $inputdir/*; do
   target_dir=$scratchdir/${recog_dir_name}
   mkdir -p $target_dir
 
-  ./recognize.sh $scratchdir/${file_id}.wav $target_dir || exit 1;
+  ./recognize.sh $scratchdir/${file_id}.wav $target_dir || fatalerror "Recognizer failed";
   cp $target_dir/${recog_dir_name}.txt $outdir/${file_id}.txt
   cp $target_dir/${recog_dir_name}.ctm $outdir/${file_id}.ctm
   ./scripts/ctm2xml.py $outdir $file_id $scratchdir
