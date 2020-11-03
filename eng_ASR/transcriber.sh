9c4baa0717c63d0fc93aaf65cf6ba28cb4a15ebf
Maarten van Gompel <proycon@anaproy.nl>
2020-11-03 13:00:17 +0100

    starting a cleanup effort

--- eng_ASR/transcriber.sh
+++ eng_ASR/transcriber.sh
@@ -40,21 +40,21 @@ fatalerror() {
 cd $resourcedir
 suffix=$(LC_CTYPE=C tr -d -c '[:alnum:]' </dev/urandom | head -c 15)
 for inputfile in $inputdir/*; do
-
   filename=$(basename "$inputfile")
+  echo "Processing $filename">&2
   extension="${filename##*.}"
-  file_id=$(basename "$inputfile" .$extension)
-  sox $inputfile -e signed-integer -c 1 -r 16000 -b 16 $scratchdir/${file_id}.wav
-  recog_dir_name=${file_id}_${suffix}
-  target_dir=$scratchdir/${recog_dir_name}
-  mkdir -p $target_dir
+  file_id=$(basename "$inputfile").$extension
+  sox $inputfile -e signed-integer -c 1 -r 16000 -b 16 "$scratchdir/${file_id}.wav" || fatalerror "sox failed"
+  recog_dir_name="${file_id}_${suffix}"
+  target_dir="$scratchdir/${recog_dir_name}"
+  mkdir -p "$target_dir"
 
-  ./recognize.sh $scratchdir/${file_id}.wav $target_dir || fatalerror "Recognizer failed";
-  cp $target_dir/${recog_dir_name}.txt $outdir/${file_id}.txt
-  cp $target_dir/${recog_dir_name}.ctm $outdir/${file_id}.ctm
-  ./scripts/ctm2xml.py $outdir $file_id $scratchdir
-  ./scripts/ctm2tg.py $outdir/${file_id}.ctm $scratchdir/${file_id}.wav
-  rm -f $scratchdir/${file_id}.wav
+  ./recognize.sh "$scratchdir/${file_id}.wav" "$target_dir" || fatalerror "Recognizer failed";
+  cp "$target_dir/${recog_dir_name}.txt" "$outdir/${file_id}.txt" || fatalerror "expected txt output not found"
+  cp "$target_dir/${recog_dir_name}.ctm" "$outdir/${file_id}.ctm" || fatalerror "expected ctm output not found"
+  ./scripts/ctm2xml.py "$outdir" "$file_id" "$scratchdir" || fatalerror "ctm2xml failed"
+  ./scripts/ctm2tg.py "$outdir/${file_id}.ctm" "$scratchdir/${file_id}.wav" || fatalerror "ctm2tg failed"
+  rm -f "$scratchdir/${file_id}.wav"
 
 done
 cd -
